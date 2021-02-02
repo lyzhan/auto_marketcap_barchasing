54fe10373a7de66d9ea2acf3592123f5e7e0471f
lyzhan <lyzhan2005@gmail.com>
2021-02-01 21:08:13 -0500

    Change unit to billion dollars

--- data_manipulate.py
+++ data_manipulate.py
@@ -2,5 +2,6 @@ import pandas as pd
 from datetime import datetime
 
 df=pd.read_table('Tesla.txt',header=None,names=['index','d','m'])
+df['m']=df['m']/10000.0
 df['date_time']=df['d'].apply(datetime.fromtimestamp)
 print(df)
