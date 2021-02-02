089b25e899abcf74b2bed02010dddc3f63a49ad8
Zhan Li <zli@pelmorex.com>
2021-02-01 21:57:16 -0500

    Add a loop for txt files

--- data_manipulate.py
+++ data_manipulate.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from datetime import datetime
+from pathlib import Path
 
-df=pd.read_table('Tesla.txt',header=None,names=['index','d','m'])
-df['m']=df['m']/10000.0
-df['date_time']=df['d'].apply(datetime.fromtimestamp)
-print(df)
+pathlist = Path('.').glob('*.txt')
+for path in pathlist:
+    # because path is object not string
+    path_in_str = str(path)
+    df=pd.read_table(path_in_str,header=None,names=['index','d','m'])
+    df['m']=df['m']/10000.0
+    df['date_time']=df['d'].apply(datetime.fromtimestamp)
