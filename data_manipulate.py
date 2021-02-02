21a1b395ee26557ce4a74bf0b51a6baafd66ef4e
lyzhan <lyzhan2005@gmail.com>
2021-02-01 22:44:48 -0500

    Create df_dump with columns equal to timelist

--- data_manipulate.py
+++ data_manipulate.py
@@ -1,8 +1,14 @@
+import numpy as np
+import datetime as dt
 import pandas as pd
 from datetime import datetime
 from pathlib import Path
 
+start=dt.datetime(2010,7,1,0,0,0)
+end=dt.datetime(2021,2,1,0,0,0)
+timelist=pd.date_range(start, end,freq='1MS')
 pathlist = Path('.').glob('*.txt')
+df_dump=pd.DataFrame(columns=timelist)
 for path in pathlist:
     # because path is object not string
     path_in_str = str(path)
